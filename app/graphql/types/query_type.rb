97db8eaec5ba023093cafa98d7f06488939bbda3
Luiz Kraisch <luizkraisch22@gmail.com>
2022-01-05 17:25:02 -0300

    Adding authentication

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,8 +5,23 @@ module Types
           null: false,
           description: "Returns a list of items in the martian library"
 
+    field :users,
+          [Types::UserType],
+          null: false,
+          description: "Returns a list of users in the martian library"
+
+    field :me, Types::UserType, null: true
+
     def items
       Item.preload(:user)
+    end
+
+    def users
+      User.all
+    end
+
+    def me
+      context[:current_user]
     end
   end
 end
--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -6,7 +6,6 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink, Observable } from 'apollo-link';
-
 export const createCache = () => {
   const cache = new InMemoryCache();
   if (process.env.NODE_ENV === 'development') {
@@ -15,16 +14,23 @@ export const createCache = () => {
   return cache;
 };
 
-// get CSRF token from meta tags
-const getToken = () =>
-  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const token = getToken();
-const setTokenForOperation = async operation =>
-  operation.setContext({
+const getTokens = () => {
+  const tokens = {
+    'X-CSRF-Token': document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute('content'),
+  };
+  const authToken = localStorage.getItem('mlToken');
+  return authToken ? { ...tokens, Authorization: authToken } : tokens;
+};
+
+const setTokenForOperation = async operation => {
+  return operation.setContext({
     headers: {
-      'X-CSRF-Token': token,
+      ...getTokens(),
     },
   });
+};
 
 // link with token
 const createLinkWithToken = () =>
@@ -49,27 +55,27 @@ const createLinkWithToken = () =>
   );
 
 // log erors
-const logError = (error) => console.error(error);
-
+const logError = error => console.error(error);
 // create error link
-const createErrorLink = () => onError(({ graphQLErrors, networkError, operation }) => {
-  if (graphQLErrors) {
-    logError('GraphQL - Error', {
-      errors: graphQLErrors,
-      operationName: operation.operationName,
-      variables: operation.variables,
-    });
-  }
-  if (networkError) {
-    logError('GraphQL - NetworkError', networkError);
-  }
-})
+const createErrorLink = () =>
+  onError(({ graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      logError('GraphQL - Error', {
+        errors: graphQLErrors,
+        operationName: operation.operationName,
+        variables: operation.variables,
+      });
+    }
+    if (networkError) {
+      logError('GraphQL - NetworkError', networkError);
+    }
+  });
 
-// http link
-const createHttpLink = () => new HttpLink({
-  uri: '/graphql',
-  credentials: 'include',
-})
+const createHttpLink = () =>
+  new HttpLink({
+    uri: '/graphql',
+    credentials: 'include',
+  });
 
 export const createClient = (cache, requestLink) => {
   return new ApolloClient({
